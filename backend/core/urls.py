c4ce1203eaa240e7801bc744d7a9edf70737f58b
Nhung Nguyet <nhungnguyet887@gmail.com>
2026-04-30 15:49:32 +0700

    update main

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include  # Nhớ thêm 'include' vào đây
-
+from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,11 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # 1. Các API về Login (JWT) giữ nguyên
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # 2. Kết nối tới file urls.py mà bạn vừa tạo thủ công trong app 'api'
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls')), 
 ]
